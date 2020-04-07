9d69f6366999adbbd887215072a8c7a070910bb2
Zain Afzal <zain.afzal@rise.cx>
2020-04-07 10:52:16 +1000

    Get first and last name fields from API and display on frontend

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const request = require('request')
+$(function () {
 
-request('https://w17sapv34a.execute-api.ap-southeast-2.amazonaws.com/Prod/getuser/1', function (error, response, body){
-    if (!error && response.statusCode == 200) {
-        console.log(body)
-      }
+    $.ajax({
+        type: 'GET',
+        url: 'https://w17sapv34a.execute-api.ap-southeast-2.amazonaws.com/Prod/getuser/1',
+        success: function(user){
+            $('#fullName').append(user.firstName + " " + user.lastName)
+        }
+    })
 })

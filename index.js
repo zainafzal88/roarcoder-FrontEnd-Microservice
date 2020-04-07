489d92b3eefd62b16b020a72a1beaed62b9bcbbb
Zain Afzal <zain.afzal@rise.cx>
2020-04-07 12:40:25 +1000

    refactored index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(function () {
         type: 'GET',
         url: 'https://w17sapv34a.execute-api.ap-southeast-2.amazonaws.com/Prod/getuser/1',
         success: function(user){
-            $('#fullName').append(user.firstName + " " + user.lastName)
+            $('#fullName').text(user.firstName + " " + user.lastName)
             $('#occupation').text(user.occupation)
             $('#email').attr('href', 'mailto:' + user.email)
             $('#linkedIn').attr('href', user.url_LinkedIn)

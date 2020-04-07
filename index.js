2ee356da72c820c7a356be191730e91a985c853d
Zain Afzal <zain.afzal@rise.cx>
2020-04-07 12:17:07 +1000

    Display user information dynamically

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ $(function () {
         url: 'https://w17sapv34a.execute-api.ap-southeast-2.amazonaws.com/Prod/getuser/1',
         success: function(user){
             $('#fullName').append(user.firstName + " " + user.lastName)
+            $('#occupation').text(user.occupation)
+            $('#email').attr('href', 'mailto:' + user.email)
+            $('#linkedIn').attr('href', user.url_LinkedIn)
+            $('#github').attr('href', user.url_Github)
         }
     })
 })
